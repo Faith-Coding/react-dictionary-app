2c2d7f4d74c36210fd8473c23a00229fb409f3f9
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-05-03 11:09:42 +0200

    Added dictionary Api integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.jpeg";
+import Dictionary from "./Dictionary";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          Coded by Akhigbe Faith
+          <a href="https://github.com/Faith-Coding/react-dictionary-app">{""}
+            Open Source{""}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
