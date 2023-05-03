ebb5760a5cc7ae6738970892d069bcac0f312566
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-05-03 16:49:35 +0200

    Render the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
+  
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_us/${keyword}`;
-    console.log(apiUrl);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -31,6 +33,8 @@ export default function Dictionary() {
           autoComplete="Off"
         />
       </form>
+    
+      <Results results={results} />
     </div>
   );
 }
